5c2c95de0cb9f4529a58c3547e2987151976629d
Jay <jay.feldman03@gmail.com>
2023-12-19 11:06:13 -0500

    do proper error messaging

--- src/lambdas/createUser.go
+++ src/lambdas/createUser.go
@@ -2,6 +2,7 @@ package lambdas
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -10,15 +11,37 @@ import (
 )
 
 func CreateUser(request events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("Received request info: %q", request.Body)
+	log.Printf("Received request info: %s", request.Body)
 
 	var person types.Person
 	err := json.Unmarshal([]byte(request.Body), &person)
 	if err != nil {
-		log.Fatalf("Error: %q", err)
+		errorMessage := fmt.Sprintf("Error unmarshalling request %s", err)
+		log.Print(errorMessage)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: errorMessage}, nil
 	}
 
-	log.Printf("Received person info: %q", person)
-	response := events.APIGatewayProxyResponse{StatusCode: 200}
+	if firstName := person.FirstName; firstName == nil {
+		errorMessage := "Missing required field: firstName"
+		log.Print(errorMessage)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: errorMessage}, nil
+	}
+
+	responseBody, err := json.Marshal(CreateUserResponse{Message: fmt.Sprintf("Hello %s", *person.FirstName)})
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error creating response body, %s", err)
+		log.Print(errorMessage)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: errorMessage}, nil
+	}
+
+	log.Printf("Received person info: %+v", person)
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseBody),
+	}
 	return response, nil
+}
+
+type CreateUserResponse struct {
+	Message string `json:"message"`
 }
